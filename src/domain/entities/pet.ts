b7f710116e4e4a076a03e97c1171324ea2b80cfc
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-24 20:09:47 -0300

    feat: add Pet entity

---
+++ src/domain/entities/pet.ts
@@ -0,0 +1,25 @@
+import { Entity } from '../../core/domain/Entity'
+
+type PetProps = {
+  name: string
+  specie: 'dog' | 'cat'
+  breed: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export class Pet extends Entity<PetProps> {
+  private constructor (props: PetProps, id?: string) {
+    super(props, id)
+  }
+
+  static create (props: PetProps, id?: string) {
+    const pet = new Pet({
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: new Date()
+    }, id)
+
+    return pet
+  }
+}
