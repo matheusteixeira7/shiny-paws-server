62cb553d7dd272221b73884cc8edfd61db5034c6
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-25 16:04:25 -0300

    feat: add update pet

---
+++ src/application/usecases/pets/update-pet.ts
@@ -0,0 +1,41 @@
+import { Customer } from '@domain/entities/customer'
+import { PetsRepository } from '@application/repositories/PetsRepository'
+
+type PetProps = {
+  id: string
+  name: string
+  specie: 'dog' | 'cat'
+  breed: string
+  owner: Customer
+}
+
+export class UpdatePet {
+  constructor (
+    private petsRepository: PetsRepository
+  ) {}
+
+  async execute ({ id, name, specie, breed, owner }: PetProps) {
+    const pet = await this.petsRepository.findById(id)
+
+    if (!pet) {
+      throw new Error('Pet not found.')
+    }
+
+    const updatedPet = Object.assign({}, pet, {
+      props: {
+        ...pet.props,
+        name,
+        specie,
+        breed,
+        owner,
+        updatedAt: new Date()
+      }
+    })
+
+    console.log(JSON.stringify(updatedPet, null, 2))
+
+    this.petsRepository.save(updatedPet)
+
+    return updatedPet
+  }
+}
