2536c3446050a6cac6dddfc8853a91ee61b2d65c
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-24 23:24:54 -0300

    feat: add in memory service repository

---
+++ tests/repositories/in-memory-services-repository.ts
@@ -0,0 +1,32 @@
+import { ServicesRepository } from '@application/repositories/ServicesRepository'
+import { Service } from '@domain/entities/service'
+
+export class InMemoryServicesRepository implements ServicesRepository {
+  public items: Service[] = []
+
+  async findById (id: string): Promise<Service | null> {
+    const service = this.items.find(service => service.id === id)
+
+    if (!service) {
+      return null
+    }
+
+    return service
+  }
+
+  async findByName (name: string): Promise<Service | null> {
+    const service = this.items.find(service => service.props.name === name)
+
+    if (!service) {
+      return null
+    }
+
+    return service
+  }
+
+  async save (service: Service): Promise<Service> {
+    this.items.push(service)
+
+    return service
+  }
+}
