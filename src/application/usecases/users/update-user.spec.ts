bccab4d420e2d116b9c6c3645e433f741885dc5f
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-25 16:16:53 -0300

    test: fix test

--- src/application/usecases/users/update-user.spec.ts
+++ src/application/usecases/users/update-user.spec.ts
@@ -15,7 +15,7 @@ describe('Update user use case', () => {
     })).rejects.toThrowError('User not found.')
   })
 
-  it('should be able to update a user', async () => {
+  it('should be able to update a user name', async () => {
     const usersRepository = new InMemoryUsersRepository()
     const sut = new UpdateUser(usersRepository)
 
@@ -27,13 +27,84 @@ describe('Update user use case', () => {
 
     await usersRepository.save(user)
 
-    await sut.execute({
+    const updatedUser = Object.assign({}, user, {
+      props: {
+        ...user.props,
+        name: 'Matheus',
+        email: 'doe@example.com',
+        password: '123456'
+      }
+    })
+
+    const result = await sut.execute({
       id: user.id,
       name: 'Matheus',
       email: 'doe@example.com',
       password: '123456'
     })
 
-    expect(user).toBeInstanceOf(User)
+    expect(result.props.name).toEqual(updatedUser.props.name)
+  })
+
+  it('should be able to update a user email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new UpdateUser(usersRepository)
+
+    const user = User.create({
+      name: 'Diego',
+      email: 'doe@example.com',
+      password: '123456'
+    })
+
+    await usersRepository.save(user)
+
+    const updatedUser = Object.assign({}, user, {
+      props: {
+        ...user.props,
+        name: 'Matheus',
+        email: 'doe@example.com',
+        password: '123456'
+      }
+    })
+
+    const result = await sut.execute({
+      id: user.id,
+      name: 'Matheus',
+      email: 'doe@example.com',
+      password: '123456'
+    })
+
+    expect(result.props.email).toEqual(updatedUser.props.email)
+  })
+
+  it('should be able to update a user password', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new UpdateUser(usersRepository)
+
+    const user = User.create({
+      name: 'Diego',
+      email: 'doe@example.com',
+      password: '123456'
+    })
+
+    await usersRepository.save(user)
+
+    const updatedUser = Object.assign({}, user, {
+      props: {
+        ...user.props,
+        name: 'Matheus',
+        email: 'doe@example.com',
+        password: '123456'
+      }
+    })
+
+    const result = await sut.execute({
+      id: user.id,
+      name: 'Matheus',
+      email: 'doe@example.com',
+      password: '123456'
+    })
+
+    expect(result.props.password).toEqual(updatedUser.props.password)
   })
 })
