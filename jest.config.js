6c8cfe37db3d492ef97144b5adb1992bf04026a5
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-25 11:01:48 -0300

    chore: change jest config extension

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+
+const pathsToModuleNameMapper = require('ts-jest/').pathsToModuleNameMapper
+const compilerOptions = require('./tsconfig.json').compilerOptions
+
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/application/usecases/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['text-summary', 'lcov'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/'
+  }),
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts']
+}
