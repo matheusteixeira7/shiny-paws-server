86a70fdd1e788b065e0c5029cfa30238a3820c4c
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-25 12:40:09 -0300

    test: fix delete user

--- src/application/usecases/users/delete-user.spec.ts
+++ src/application/usecases/users/delete-user.spec.ts
@@ -1,18 +1,14 @@
 import { User } from '@domain/entities/user'
 import { InMemoryUsersRepository } from '@tests/repositories/in-memory-users-repository'
 import { DeleteUser } from './delete-user'
-import { UpdateUser } from './update-user'
 
 describe('Delete user use case', () => {
   it('should throw error if user not found exists', async () => {
     const usersRepository = new InMemoryUsersRepository()
-    const sut = new UpdateUser(usersRepository)
+    const sut = new DeleteUser(usersRepository)
 
     await expect(sut.execute({
-      id: '1',
-      name: 'Diego',
-      email: 'doe@example.com',
-      password: '123456'
+      id: '1'
     })).rejects.toThrowError('User not found.')
   })
 
