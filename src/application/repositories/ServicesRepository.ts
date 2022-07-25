55b390a8be4b19cb03998d9bc9675b01bd0d538b
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-24 23:24:46 -0300

    feat: add service repository

---
+++ src/application/repositories/ServicesRepository.ts
@@ -0,0 +1,7 @@
+import { Service } from '@domain/entities/service'
+
+export interface ServicesRepository {
+  findById(id: string): Promise<Service | null>
+  findByName(name: string): Promise<Service | null>
+  save (customer: Service): Promise<Service>
+}
