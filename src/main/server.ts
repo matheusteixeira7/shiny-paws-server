f6ec600e75aa44d84814fa4a7835827a368ee7d1
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-28 18:30:50 -0300

    chore: add express-async-errors

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,15 +2,20 @@ import 'reflect-metadata'
 import 'module-alias/register'
 import 'dotenv/config'
 import '@infra/container'
+import 'express-async-errors'
 
 import cors from 'cors'
 import routes from './routes'
-import express from 'express'
+import express, { json } from 'express'
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(json())
 app.use(routes)
+app.use((req, res, next) => {
+  res.type('json')
+  next()
+})
 
 app.listen(3333, () => {
   console.log('Server started on port 3333')
