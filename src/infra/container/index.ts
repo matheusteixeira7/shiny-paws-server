140dd74e0a90b8684835337d0154cacc76ba3c9a
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-28 20:09:40 -0300

    feat: add customers repository

--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -1,5 +1,8 @@
+import { CustomersRepository } from '@application/repositories/CustomersRepository'
 import { UsersRepository } from '@application/repositories/UsersRepository'
+import { InMemoryCustomersRepository } from '@tests/repositories/in-memory-customers-repository'
 import { InMemoryUsersRepository } from '@tests/repositories/in-memory-users-repository'
 import { container } from 'tsyringe'
 
 container.registerSingleton<UsersRepository>('InMemoryUsersRepository', InMemoryUsersRepository)
+container.registerSingleton<CustomersRepository>('InMemoryCustomersRepository', InMemoryCustomersRepository)
