8ed604c2de5d67f707f1a0eca6da770c2631bb7b
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-28 20:32:03 -0300

    feat: add users routes

--- src/main/routes/users.routes.ts
+++ src/main/routes/users.routes.ts
@@ -5,4 +5,7 @@ export const usersRouter = Router()
 const usersController = new UsersController()
 
 usersRouter.get('/', usersController.list)
+usersRouter.get('/:id', usersController.get)
 usersRouter.post('/', usersController.create)
+usersRouter.put('/:id', usersController.update)
+usersRouter.delete('/:id', usersController.delete)
