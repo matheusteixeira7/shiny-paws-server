ca234446dc2ebee8b9274a8230f98fcf644a1fb2
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-29 09:34:26 -0300

    feat: add session route

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
+import { sessionRouter } from './session.routes'
 import { usersRouter } from './users.routes'
 
 export const routes = Router()
 
 routes.use('/users', usersRouter)
+routes.use('/session', sessionRouter)
 
 export default routes
