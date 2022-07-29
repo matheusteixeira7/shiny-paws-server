6392219b461c7517ee655c7071524feeb0fead65
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-29 10:11:23 -0300

    feat: register more in memory repositories

--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -1,8 +1,14 @@
 import { CustomersRepository } from '@application/repositories/CustomersRepository'
+import { PetsRepository } from '@application/repositories/PetsRepository'
+import { ServicesRepository } from '@application/repositories/ServicesRepository'
 import { UsersRepository } from '@application/repositories/UsersRepository'
 import { InMemoryCustomersRepository } from '@tests/repositories/in-memory-customers-repository'
+import { InMemoryPetsRepository } from '@tests/repositories/in-memory-pets-repository'
+import { InMemoryServicesRepository } from '@tests/repositories/in-memory-services-repository'
 import { InMemoryUsersRepository } from '@tests/repositories/in-memory-users-repository'
 import { container } from 'tsyringe'
 
-container.registerSingleton<UsersRepository>('InMemoryUsersRepository', InMemoryUsersRepository)
 container.registerSingleton<CustomersRepository>('InMemoryCustomersRepository', InMemoryCustomersRepository)
+container.registerSingleton<PetsRepository>('InMemoryPetsRepository', InMemoryPetsRepository)
+container.registerSingleton<ServicesRepository>('InMemoryServicesRepository', InMemoryServicesRepository)
+container.registerSingleton<UsersRepository>('InMemoryUsersRepository', InMemoryUsersRepository)
