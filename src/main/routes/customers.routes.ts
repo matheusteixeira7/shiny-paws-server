8e8e7bf0c75eb0bf142905975f19f3564e7176b3
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-30 14:28:35 -0300

    feat: add celebrate validation

--- src/main/routes/customers.routes.ts
+++ src/main/routes/customers.routes.ts
@@ -1,11 +1,52 @@
 import { Router } from 'express'
 import { CustomersController } from '@infra/controllers'
+import { celebrate, Joi, Segments } from 'celebrate'
 
 export const customersRouter = Router()
 const customersController = new CustomersController()
 
 customersRouter.get('/', customersController.list)
-customersRouter.post('/', customersController.create)
-customersRouter.delete('/:id', customersController.delete)
-customersRouter.get('/:id', customersController.get)
-customersRouter.put('/:id', customersController.update)
+
+customersRouter.post('/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      phone: Joi.string().required(),
+      address: Joi.string().required()
+    }
+  }),
+  customersController.create
+)
+
+customersRouter.delete('/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    }
+  }),
+  customersController.delete
+)
+customersRouter.get('/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    }
+  }),
+  customersController.get
+)
+
+customersRouter.put('/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      phone: Joi.string().required(),
+      address: Joi.string().required()
+    }
+  }),
+  customersController.update
+)
