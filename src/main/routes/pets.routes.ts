0e5e2e0ef061582981c2fba2cf527e1145b603b2
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-30 13:21:22 -0300

    feat: implement pets routes

--- src/main/routes/pets.routes.ts
+++ src/main/routes/pets.routes.ts
@@ -5,3 +5,7 @@ export const petsRouter = Router()
 const petsController = new PetsController()
 
 petsRouter.get('/', petsController.list)
+petsRouter.post('/', petsController.create)
+petsRouter.post('/:id', petsController.get)
+petsRouter.delete('/:id', petsController.delete)
+petsRouter.put('/:id', petsController.update)
