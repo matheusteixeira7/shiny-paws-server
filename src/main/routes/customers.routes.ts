d26b927f1cb5be922b2a7b281bbb2acc7fea169d
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-30 13:21:14 -0300

    feat: add customers routes

--- src/main/routes/customers.routes.ts
+++ src/main/routes/customers.routes.ts
@@ -5,5 +5,7 @@ export const customersRouter = Router()
 const customersController = new CustomersController()
 
 customersRouter.get('/', customersController.list)
-
 customersRouter.post('/', customersController.create)
+customersRouter.delete('/:id', customersController.delete)
+customersRouter.get('/:id', customersController.get)
+customersRouter.put('/:id', customersController.update)
