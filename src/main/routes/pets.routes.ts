f6ab9e8bd6ba0913e3d34e8b8a347d5e06e5bf08
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-30 14:23:55 -0300

    feat: add pets routes

--- src/main/routes/pets.routes.ts
+++ src/main/routes/pets.routes.ts
@@ -1,11 +1,53 @@
 import { Router } from 'express'
+import { celebrate, Joi, Segments } from 'celebrate'
 import { PetsController } from '@infra/controllers/pets-controller'
 
 export const petsRouter = Router()
 const petsController = new PetsController()
 
 petsRouter.get('/', petsController.list)
-petsRouter.post('/', petsController.create)
-petsRouter.post('/:id', petsController.get)
-petsRouter.delete('/:id', petsController.delete)
-petsRouter.put('/:id', petsController.update)
+
+petsRouter.post('/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      specie: Joi.string().required(),
+      breed: Joi.string().required(),
+      ownerId: Joi.string().required()
+    }
+  }),
+  petsController.create
+)
+
+petsRouter.get('/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    }
+  }),
+  petsController.get
+)
+
+petsRouter.delete('/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    }
+  }),
+  petsController.delete
+)
+
+petsRouter.put('/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      specie: Joi.string().required(),
+      breed: Joi.string().required(),
+      ownerId: Joi.string().required()
+    }
+  }),
+  petsController.update
+)
