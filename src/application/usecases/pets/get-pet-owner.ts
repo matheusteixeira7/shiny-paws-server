356ca3da31be39aa438bb265267107dc8e33d670
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-30 21:03:45 -0300

    feat: add dependency injection

--- src/application/usecases/pets/get-pet-owner.ts
+++ src/application/usecases/pets/get-pet-owner.ts
@@ -1,9 +1,14 @@
 import { InvalidParamError } from '@application/errors'
 import { CustomersRepository } from '@application/repositories'
 import { Customer } from '@domain/entities'
+import { inject, injectable } from 'tsyringe'
 
+@injectable()
 export class GetPetOwner {
-  constructor (private customersRepository: CustomersRepository) {}
+  constructor (
+    @inject('InMemoryCustomersRepository')
+    private customersRepository: CustomersRepository
+  ) {}
 
   async execute (id: string): Promise<Customer> {
     const owner = await this.customersRepository.findById(id)
