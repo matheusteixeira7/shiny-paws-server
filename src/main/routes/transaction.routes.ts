58717ccdea2135b0ea5f12f399ef806cd1b96473
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-20 13:57:22 -0300

    feat: add routes to update and delete a transaction

--- src/main/routes/transaction.routes.ts
+++ src/main/routes/transaction.routes.ts
@@ -9,3 +9,7 @@ transactionsRouter.get('/', transactionsController.list)
 transactionsRouter.post('/', transactionsController.create)
 
 transactionsRouter.get('/:id', transactionsController.get)
+
+transactionsRouter.put('/:id', transactionsController.update)
+
+transactionsRouter.delete('/:id', transactionsController.delete)
