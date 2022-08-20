bd2e5ed2a44855b065a4ed2040371daf1e149bb3
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-20 18:31:29 -0300

    feat: add isAuthenticated to route and validation

--- src/main/routes/transaction.routes.ts
+++ src/main/routes/transaction.routes.ts
@@ -1,15 +1,57 @@
+import { isAuthenticated } from '@main/middlewares/is-authenticated'
 import { TransactionsController } from '@infra/controllers'
 import { Router } from 'express'
+import { celebrate, Joi, Segments } from 'celebrate'
 
 export const transactionsRouter = Router()
 const transactionsController = new TransactionsController()
 
-transactionsRouter.get('/', transactionsController.list)
+transactionsRouter.get('/', isAuthenticated, transactionsController.list)
 
-transactionsRouter.post('/', transactionsController.create)
+transactionsRouter.post('/',
+  isAuthenticated,
+  celebrate({
+    [Segments.BODY]: {
+      servicesIds: Joi.string().required(),
+      totalPrice: Joi.number().required(),
+      isPaid: Joi.boolean().required(),
+      customerId: Joi.string().required()
+    }
+  }),
+  transactionsController.create
+)
 
-transactionsRouter.get('/:id', transactionsController.get)
+transactionsRouter.get('/:id',
+  isAuthenticated,
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    }
+  }), transactionsController.get
+)
 
-transactionsRouter.put('/:id', transactionsController.update)
+transactionsRouter.put('/:id',
+  isAuthenticated,
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    },
+    [Segments.BODY]: {
+      servicesIds: Joi.string().required(),
+      totalPrice: Joi.number().required(),
+      isPaid: Joi.boolean().required(),
+      customerId: Joi.string().required()
+    }
+  }),
+  transactionsController.update
+)
 
-transactionsRouter.delete('/:id', transactionsController.delete)
+transactionsRouter.delete('/:id',
+  isAuthenticated,
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    }
+  }),
+  transactionsController.delete
+)
